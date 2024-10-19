b5bb03e0cacdb667bdc6cf4d7470332f914a5844
Ro-hit16 <rohitdevkar301@gmail.com>
2024-10-19 10:43:12 +0530

    first commit

---
+++ Trie_implementation.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+
+class TrieNode {
+public:
+    char data;
+    TrieNode* children[26];
+    bool isTerminal;
+
+    TrieNode(char ch) {
+        data = ch;
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode('\0');
+    }
+
+    void insertUtil(TrieNode* root, string word) {
+        if (word.length() == 0) {
+            root->isTerminal = true;
+            return;
+        }
+
+        int index = word[0] - 'A';
+        TrieNode* child;
+
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        } else {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+
+        insertUtil(child, word.substr(1));
+    }
+
+    void insertWord(string word) {
+        insertUtil(root, word);
+    }
+
+    bool searchUtil(TrieNode* root, string word) {
+        if (word.length() == 0) {
+            return root->isTerminal;
+        }
+
+        int index = word[0] - 'A';
+        TrieNode* child;
+
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        } else {
+            return false;
+        }
+
+        return searchUtil(child, word.substr(1));
+    }
+
+    bool searchWord(string word) {
+        return searchUtil(root, word);
+    }
+
+    bool deleteUtil(TrieNode* root, string word) {
+        if (word.length() == 0) {
+            if (root->isTerminal) {
+                root->isTerminal = false;
+                return true;
+            }
+            return false;
+        }
+
+        int index = word[0] - 'A';
+        TrieNode* child;
+
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        } else {
+            return false;
+        }
+
+        bool canDeleteChild = deleteUtil(child, word.substr(1));
+
+        if (canDeleteChild) {
+            delete child;
+            root->children[index] = NULL;
+            for (int i = 0; i < 26; i++) {
+                if (root->children[i] != NULL) {
+                    return false;
+                }
+            }
+            return !root->isTerminal;
+        }
+        return false;
+    }
+
+    void deleteWord(string word) {
+        deleteUtil(root, word);
+    }
+};
+
+int main() {
+    Trie* t = new Trie();
+    t->insertWord("HELLO");
+    t->insertWord("HEY");
+
+    cout << t->searchWord("HELLO") << endl; 
+    cout << t->searchWord("HEY") << endl;   
+    cout << t->searchWord("HI") << endl;    
+
+    t->deleteWord("HEY");
+    cout << t->searchWord("HEY") << endl;  
+
+    return 0;
+}
